053b0e2716ac254146ef125069c2f694f365a501
Hardik Kamaliya <hardikkamaliya0000@gmail.com>
2023-08-04 15:36:34 +0530

    add php

--- form-handler.php
+++ form-handler.php
@@ -4,22 +4,26 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_form = 'principal@zspatelcollege.com'
+$email_form = 'principal@zspatelcollege.com';
 
-$email_subject = 'New Form Submission'
+$email_subject = 'New Form';
 
 $email_body = "User Name: $name.\n".
-              "User Email: $visitor_email.\n"
-              "Subject: $subject.\n"
+              "User Email: $visitor_email.\n".
+              "Subject: $subject.\n".
               "User Message: $message.\n";
 
-$to = 'hardikkamaliya0000@gmail.com'
+
+$to = 'hardikkamaliya0000@gmail.com';
 
 $headers = "From: $email_form \r\n";
 
-$headers = "Ready-To: $visitor_email \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to,$email_subject_body,$headers)
+mail($to,$email_subject,$email_body,$headers);
 
-header("Location: contact.html")
+header("Location: contact.html");
+
+
+
 ?>
